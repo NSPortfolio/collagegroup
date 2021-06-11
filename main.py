2d8251fab01d4f9e866168715025c43c2ed80838
Times4Everyone <76495035+Times4Everyone@users.noreply.github.com>
2021-06-11 18:34:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,4 @@ class SecondPage(MethodView):
         question_page = QuestionForm()
         return render_template('second_page.html', questionpage=question_page)
 
-
-app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
-app.add_url_rule('/select', view_func=SecondPage.as_view('second_page'))
-
 app.run()
