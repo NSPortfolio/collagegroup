2596d43749ae331217a1e9e33f3fb32d6eb57db1
Times4Everyone <76495035+Times4Everyone@users.noreply.github.com>
2021-06-13 13:50:44 -0500

    Update searchingfiles.py

--- searchingfiles.py
+++ searchingfiles.py
@@ -4,9 +4,11 @@ def searchtool(file):
     df = pd.read_csv(file, usecols = ['Name'])
     df = df.Name.to_list()
     for x in searchtool('https://raw.githubusercontent.com/Times4Everyone/collagegroup/main/tags.csv'):
-      x=x
+      print(x)
     html = '<form><input list="tags" name="tags"><datalist>'
     for options in x:
         html += '<option value=' + str(options) + '>'
     html += ' </datalist><a href = "getstarted.html" class="button"> Search </a></center></form>'
+    webpage = open("names.txt","w+")
     webpage.write(html)
+    return(html)
